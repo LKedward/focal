ccedc7d21c4384bb348fceb95d756aa1fe2bc56d
LKedward <laurence.kedward@bristol.ac.uk>
2019-12-27 16:25:57 +0000

    Minor cleanup: of nbody example program. Removed use of vector type float4 in kernel. Removed saving of particle positions to tecplot.

--- examples/nbody.cl
+++ examples/nbody.cl
@@ -1,25 +1,25 @@
 #define SOFT 1e-9f
 
 __kernel void bodyForces(const int nBody, const float dt, 
-            __global float4 * px, __global float4 * py, __global float4 * pz,
-            __global float4 * vx, __global float4 * vy, __global float4 * vz){
+            __global float * px, __global float * py, __global float * pz,
+            __global float * vx, __global float * vy, __global float * vz){
  
   int i = get_global_id(0);
   if (i < nBody) {
       
-    float4 Fx = 0.0f;
-    float4 Fy = 0.0f;
-    float4 Fz = 0.0f;
+    float Fx = 0.0f;
+    float Fy = 0.0f;
+    float Fz = 0.0f;
 
     for (int j=0; j<nBody; j++){
 
-      float4 dx = px[j] - px[i];
-      float4 dy = py[j] - py[i];
-      float4 dz = pz[j] - pz[i];
+      float dx = px[j] - px[i];
+      float dy = py[j] - py[i];
+      float dz = pz[j] - pz[i];
 
-      float4 distSqr = dx*dx + dy*dy + dz*dz + SOFT;
-      float4 invDist = rsqrt(distSqr);
-      float4 invDist3 = invDist * invDist * invDist;
+      float distSqr = dx*dx + dy*dy + dz*dz + SOFT;
+      float invDist = rsqrt(distSqr);
+      float invDist3 = invDist * invDist * invDist;
 
       Fx += dx * invDist3;
       Fy += dy * invDist3;
@@ -36,8 +36,8 @@ __kernel void bodyForces(const int nBody, const float dt,
 }
 
 __kernel void integrateBodies(const int nBody, const float dt, 
-            __global float4 * px, __global float4 * py, __global float4 * pz,
-            __global float4 * vx, __global float4 * vy, __global float4 * vz){
+            __global float * px, __global float * py, __global float * pz,
+            __global float * vx, __global float * vy, __global float * vz){
 
   int i = get_global_id(0);
   if (i < nBody) {
--- examples/nbody.f90
+++ examples/nbody.f90
@@ -1,89 +1,72 @@
-module nBodyUtils
-  implicit none
-  contains
-  subroutine tecplotDump(fh,px,py,pz,t)
-    integer, intent(in) :: fh
-    real, intent(in), dimension(:) :: px, py, pz
-    real, intent(in) :: t
-
-    integer :: i, n
-    n = size(px,1)
-
-    write(fh,*) 'ZONE I=',n,' DATAPACKING=POINT SOLUTIONTIME=',t
-
-    do i=1,n
-      write(fh,*) px(i), py(i), pz(i)
-    end do
-
-  end subroutine tecplotDump
-  ! -----------------------------------------------------------------------------
-
-  subroutine tecplotStart(fh,filename)
-    integer, intent(out) :: fh
-    character(*), intent(in) :: filename
-
-    open(newunit=fh, file=filename, status="replace")
-
-    write(fh,*) 'TITLE="nBody"'
-    write(fh,*) 'VARIABLES = "X" "Y" "Z"'
-
-  end subroutine tecplotStart
-  ! -----------------------------------------------------------------------------
-end module nBodyUtils
-
 program nbody
 !! Focal example program: simple gravitational n-body simulator
 !! Based on: https://github.com/ROCm-Developer-Tools/HIP-Examples/blob/master/mini-nbody/cuda/nbody-orig.cu
-use nBodyUtils
+!!
+!! This program demonstrates the following Focal capabilities
+!!  - Creating a default context and selecting a device
+!!  - Loading kernels from compiled resource
+!!  - Transferring data to the device
+!!  - Out-of-order command queue with barriers and event-dependencies
+!!  - Profiling kernels and buffer transfers
+!!
 use Focal
-use clfortran
 implicit none
 
-integer :: N = 5000 !3E5                          ! No. of bodies
-integer :: blockSize = 256
-real :: dt = 1                           ! Global time-step
-integer :: Niter = 5000                      ! Number of iterations to perform
-integer :: saveFreq = 500
+! --------- Program configuration ---------
+integer, parameter :: N = 5000 !3E5                 ! No. of bodies
+integer, parameter :: blockSize = 256               ! Local work group size
+real, parameter :: dt = 1                           ! Global time-step
+integer, parameter :: Niter = 1000                  ! Number of iterations to perform
+character(*), parameter :: cl_vendor = 'nvidia'     ! Vendor for which to create OpenCL context
 
-integer :: fh
-integer :: i, nBlock, temp
-character(:), allocatable :: kernelSrc      ! Kernel source string
-type(fclDevice), allocatable :: devices(:)      ! List of focal devices
-type(fclProgram) :: prog                    ! Focal program object 
-type(fclKernel) :: kern1, kern2             ! Focal kernel object
+! ---------Program variables  ---------
+integer :: i, nBlock
+integer :: kern1T, kern2T
+real :: Tavg, perf
+character(:), allocatable :: kernelSrc              ! Kernel source string
+type(fclDevice), allocatable :: devices(:)          ! List of focal devices
+type(fclProgram) :: prog                            ! Focal program object 
+type(fclKernel) :: kern1, kern2                     ! Focal kernel object
 type(fclEvent) :: e
 
-real, allocatable, dimension(:) :: px, py, pz, vx, vy, vz
+real, dimension(N) :: px, py, pz, vx, vy, vz
 type(fclDeviceFloat) :: pxd, pyd, pzd, vxd, vyd, vzd
 
+! Write header
+write(*,*) ('-',i=1,72)
+write(*,*) '                Focal example program: nbody simulator'
+write(*,'(A,I6,A,I6,A)') '                 ( NBody: ',N,'    NIteration: ',Niter,')'
+write(*,*) ('-',i=1,72)
+write(*,*)
+
 ! Create context with nvidia platform
-call fclSetDefaultContext(fclCreateContext(vendor='nvidia'))
+call fclSetDefaultContext(fclCreateContext(vendor=cl_vendor))
 
 ! Select device with most cores and create command queue
 devices = fclFindDevices(sortBy='cores')
 
 call fclSetDefaultCommandQ(fclCreateCommandQ(devices(1),enableProfiling=.true., &
-           outOfOrderExec=.true.,blockingWrite=.false.,blockingRead=.false.))
+           outOfOrderExec=.true.,blockingWrite=.false.))
 
-! Load kernel from file and compile
-! call fclSourceFromFile('examples/sum.cl',kernelSrc)
+write(*,*) '  Created OpenCL command queue on device: "',devices(1)%name,'"'
+write(*,'(A,I6,A,I6,A,I4,A,A,A)') '    (', devices(1)%nComputeUnits,' cores, ', &
+    devices(1)%global_memory/1024/1024,'MB, ', &
+    devices(1)%clock_freq, 'MHz, ',&
+    devices(1)%version,')'
+write(*,*) ''
+
+! Load kernels from file and compile
 call fclGetKernelResource(kernelSrc)
 prog = fclCompileProgram(kernelSrc)
-kern1 = fclGetProgramKernel(prog,'bodyForces')
-kern2 = fclGetProgramKernel(prog,'integrateBodies')
+
+! Get kernel objects and set local/global work sizes
+nBlock = (N+blockSize-1)/blockSize
+kern1 = fclGetProgramKernel(prog,'bodyForces',[nBlock*blockSize],[blockSize],&
+                              profileSize=Niter)
+kern2 = fclGetProgramKernel(prog,'integrateBodies',[nBlock*blockSize],[blockSize],&
+                              profileSize=Niter)
 
 ! Initialise host array data
-allocate(px(N))
-allocate(py(N))
-allocate(pz(N))
-allocate(vx(N))
-allocate(vy(N))
-allocate(vz(N))
-
-vx(:) = 0
-vy(:) = 0
-vz(:) = 0
-
 call random_number(vx)
 call random_number(vy)
 call random_number(vz)
@@ -92,21 +75,13 @@ call random_number(px)
 call random_number(py)
 call random_number(pz)
 
-px = px*1E8
-py = py*1E8
-pz = pz*1E8
-
-vx = vx*1E2
-vy = vy*1E2
-vz = vz*1E2
-
 ! Initialise device arrays
-pxd = fclBufferFloat(N,read=.true.,write=.true.)
-pyd = fclBufferFloat(N,read=.true.,write=.true.)
-pzd = fclBufferFloat(N,read=.true.,write=.true.)
-vxd = fclBufferFloat(N,read=.true.,write=.true.)
-vyd = fclBufferFloat(N,read=.true.,write=.true.)
-vzd = fclBufferFloat(N,read=.true.,write=.true.)
+pxd = fclBufferFloat(N,read=.true.,write=.true.,profileSize=1,profileName='pxd')
+pyd = fclBufferFloat(N,read=.true.,write=.true.,profileSize=1,profileName='pyd')
+pzd = fclBufferFloat(N,read=.true.,write=.true.,profileSize=1,profileName='pzd')
+vxd = fclBufferFloat(N,read=.true.,write=.true.,profileSize=1,profileName='vxd')
+vyd = fclBufferFloat(N,read=.true.,write=.true.,profileSize=1,profileName='vyd')
+vzd = fclBufferFloat(N,read=.true.,write=.true.,profileSize=1,profileName='vzd')
 
 ! Copy data to device
 pxd = px
@@ -118,51 +93,36 @@ vzd = vz
 call fclBarrier()
 e = fclLastBarrierEvent
 
-nBlock = (N/4+blockSize-1)/blockSize
-
-! Set local and global work sizes
-kern1%local_work_size(1) = blockSize
-kern1%global_work_size(1) = nBlock*blockSize
-kern2%local_work_size(1) = blockSize
-kern2%global_work_size(1) = nBlock*blockSize
-
-! call fclWait()
-
-! read(*,*)
-
-call tecplotStart(fh,'nBody.plt')
-
 ! Set kernel arguments once
 call kern1%setArgs(N,dt,pxd,pyd,pzd,vxd,vyd,vzd)
 call kern2%setArgs(N,dt,pxd,pyd,pzd,vxd,vyd,vzd)
 
+! Main time-stepping loop
+write(*,'(A)',advance='no') '  Time-stepping...'
 do i=1,nIter
 
   call kern1%launchAfter(e)
   call kern2%launchAfter(fclLastKernelEvent)
-
-  if (mod(i,saveFreq)==0) then
-    write(*,*) 'iter ',i
-
-    call fclSetDependency(fclLastKernelEvent,hold=.true.)
-    px = pxd 
-    py = pyd
-    pz = pzd
-    call fclClearDependencies()
-    call fclBarrier()
-
-    call fclWait(fclLastBarrierEvent)
-    call tecplotDump(fh,px,py,pz,i*dt)
-  end if
-
-  ! call fclWait()
-
-  call fclBarrier()
-  e = fclLastBarrierEvent
+  e = fclLastKernelEvent
 
 end do
 
-temp = clFinish(fclDefaultCmdQ%cl_command_queue)
+call fclWait()
+write(*,*) ' done.'
+
+
+! Calculate and print out profiling data
+call fclDumpProfileData([kern1,kern2],devices(1))
+call fclDumpProfileData([pxd,pyd,pzd,vxd,vyd,vzd])
+
+! Calculate performance metric
+kern1T = sum(fclGetEventDurations(kern1%profileEvents(1:Niter)))
+kern2T = sum(fclGetEventDurations(kern2%profileEvents(1:Niter)))
+Tavg = (kern1T+kern2T)/NIter
+
+perf = N*N/Tavg
+
+write(*,*) perf,' billion interactions per second'
 
 end program nbody
 ! -----------------------------------------------------------------------------
