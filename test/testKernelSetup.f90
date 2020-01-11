f398c603acd1cecd18ea5905ea669e90fee34fef
Laurence Kedward <laurence.kedward@bristol.ac.uk>
2020-01-11 16:07:49 +0000

    Add: simple kernel launch test. This test launches simple kernels which set values in device buffers then transfers the buffers to host and verifies the contents of them.

---
+++ test/testKernelSetup.f90
@@ -0,0 +1,72 @@
+program testKernelSetup
+!! Focal test program
+!!
+!! This test launches simple kernels which set int/float/double device buffers
+!!  then transfers the buffers to host and verifies the contents of them.
+
+use Focal
+use Focal_Test_Utils
+use iso_fortran_env, only: sp=>real32, dp=>real64
+implicit none
+
+character(*), parameter :: test_string = 'Hello world'
+
+character(:), allocatable :: kernelSrc              ! Kernel source string
+type(fclProgram) :: prog                            ! Focal program object
+type(fclKernel) :: setInt_k, setFloat_k, setDouble_k
+
+real(sp), dimension(FCL_TEST_SIZE) :: hostReal32
+real(dp), dimension(FCL_TEST_SIZE) :: hostReal64
+integer, dimension(FCL_TEST_SIZE) :: hostInt32
+character(len=1), dimension(len(test_string)), target :: hostChar
+
+type(fclDeviceFloat) :: deviceReal32
+type(fclDeviceDouble) :: deviceReal64
+type(fclDeviceInt32) :: deviceInt32
+type(fclDeviceBuffer) :: deviceBuffer
+
+integer :: i
+
+! --- Initialise ---
+call fclTestInit()
+
+! --- Initialise typed device buffers ---
+deviceInt32 = fclBufferInt32(FCL_TEST_SIZE,read=.true.,write=.false.)
+deviceReal32 = fclBufferFloat(FCL_TEST_SIZE,read=.true.,write=.false.)
+deviceReal64 = fclBufferDouble(FCL_TEST_SIZE,read=.true.,write=.false.)
+
+! --- Manually initialise un-typed buffer objects ---
+deviceBuffer%cmdq => fclDefaultCmdQ
+deviceBuffer%nBytes = c_sizeof(hostChar)
+deviceBuffer%cl_mem = fclBuffer(fclDefaultCmdQ,c_sizeof(hostChar),read=.true.,write=.true.)
+
+! --- Initialise kernels ---
+call fclGetKernelResource(kernelSrc)
+prog = fclCompileProgram(kernelSrc)
+setInt_k = fclGetProgramKernel(prog,'setInt32Test',[FCL_TEST_SIZE])
+setFloat_k = fclGetProgramKernel(prog,'setFloatTest',[FCL_TEST_SIZE])
+setDouble_k = fclGetProgramKernel(prog,'setDoubleTest',[FCL_TEST_SIZE])
+
+! --- Call kernels ---
+call fclLaunchKernel(setInt_k,FCL_TEST_SIZE,deviceInt32)
+call fclLaunchKernel(setFloat_k,FCL_TEST_SIZE,deviceReal32)
+call fclLaunchKernel(setDouble_k,FCL_TEST_SIZE,deviceReal64)
+
+! --- Transfer device buffers to host ---
+hostInt32 = deviceInt32
+hostReal32 = deviceReal32
+hostReal64 = deviceReal64
+
+! --- Check arrays ---
+call fclTestAssertEqual([sum(hostInt32)],[sum([(i,i=0,FCL_TEST_SIZE-1)])],'sum(hostInt32)')
+call fclTestAssertEqual([sum(hostReal32)],[sum([(1.0*i,i=0,FCL_TEST_SIZE-1)])],'sum(hostReal32)')
+call fclTestAssertEqual([sum(hostReal64)],[sum([(1.0d0*i,i=0,FCL_TEST_SIZE-1)])],'sum(hostReal64)')
+
+call fclFreeBuffer(deviceInt32)
+call fclFreeBuffer(deviceReal32)
+call fclFreeBuffer(deviceReal64)
+
+call fclTestFinish()
+
+end program testKernelSetup
+! -----------------------------------------------------------------------------
